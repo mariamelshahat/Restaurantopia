59c4be1a48f5202959054b01ea375e0703603a5a
MaryamAshraf4 <maryamashraf386@gmail.com>
2024-10-11 19:10:41 +0300

    New

---
+++ Restaurantopia/Controllers/ItemController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Restaurantopia.Entities.Models;
+using Restaurantopia.InterFaces;
+
+namespace Restaurantopia.Controllers
+{
+    public class ItemController : Controller
+    {
+        private IGenericRepository<Item> _repository;
+
+        public ItemController(IGenericRepository<Item> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ActionResult> ListOfItems()
+        {
+            var Items = await _repository.GetAllAsync();
+            return View(Items);
+        }
+
+        public ActionResult Details(int id)
+        {
+            
+            return View();
+        }
+
+
+        public async Task<ActionResult> Create()
+        {           
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ItemController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ItemController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ItemController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ItemController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
